d1423b17f0d67e0cc83d1dc9051f9ff1a8b378d3
sztangcy <your email>
2021-05-26 11:06:45 +0800

    first edition

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by YMXD on 2021/3/23.
+#include<bits/stdc++.h>
+#include <staff_member.h>
+using namespace std;
+
+
+
+int main() {
+  StaffMember staff_member("name", "address", "phone");
+  cout << staff_member.tostring();
+  //cout << staff_member.pay();
+  return 0;
+}
+//
+
